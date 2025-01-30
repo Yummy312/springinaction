6e4c09350ada8f6a0a5c8513ba240e6a9c7a2e0d
Yummy312 <kurmushuev.nurzhigit@gmail.com>
2025-01-30 21:57:58 +0600

    Обработка коллекций на языке SpEL

---
+++ src/main/java/com/springinaction/springidol/Location.java
@@ -0,0 +1,7 @@
+package com.springinaction.springidol;
+
+public interface Location {
+    String getName();
+    String getState();
+    int getPopulation();
+}
--- src/main/java/com/springinaction/springidol/SpringinactionApplication.java
+++ src/main/java/com/springinaction/springidol/SpringinactionApplication.java
@@ -3,14 +3,16 @@ import com.springinaction.springidol.exceptions.PerformanceException;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.List;
+
 public class SpringinactionApplication {
 
 	public static void main(String[] args) throws PerformanceException {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"spring-idol.xml");
 
-		Instrumentalist carl = context.getBean("carl", Instrumentalist.class);
-		carl.perform();
+		List<City> cities = context.getBean("cities", List.class);
+		System.out.println(cities.get(1).getName());
 
 	}
 
