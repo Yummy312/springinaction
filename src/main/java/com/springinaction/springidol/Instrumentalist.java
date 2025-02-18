75c3aaca3c1913ecd6be85b137b4d7b0fba966b7
Yummy312 <kurmushuev.nurzhigit@gmail.com>
2025-02-18 20:47:45 +0600

    Связывание посредством аннотаций: Использование выражений в аннотациях внедрения зависимостей

--- src/main/java/com/springinaction/springidol/Instrumentalist.java
+++ src/main/java/com/springinaction/springidol/Instrumentalist.java
@@ -1,7 +1,6 @@
 package com.springinaction.springidol;
 import com.springinaction.springidol.exceptions.PerformanceException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 
 
 public class Instrumentalist implements Performer {
@@ -13,9 +12,20 @@ public class Instrumentalist implements Performer {
         instrument.play();
     }
     private String song;
+
+//    Помимо внедрения ссылок на компоненты с помощью аннотаций автоматического связывания,
+//    может появиться желание использовать аннотации для внедрения простых значений. В версии Spring
+//    3.0 появилась новая аннотация @Value , позволяющая внедрять простые значения таких типов,
+//    как int, boolean и String. Например:
+    @Value("Jingle Bells")  // Здесь в строковое свойство внедряется обычное строковое значение
     public void setSong(String song) { // Внедрение мелодии
         this.song = song;
     }
+//    Аннотация @Value проста в использовании. Ее применение заключается в том, чтобы добавить аннотацию
+//    @Value перед свойством, методом или параметром метода и передать ей строку с выражением,
+//    результат которого должен быть внедрен.
+
+
     public String getSong() {
         return song;
     }
@@ -25,16 +35,13 @@ public class Instrumentalist implements Performer {
     private Instrument instrument;
 
 
-    @Autowired(required = false)
+//    Внедрение простых значений не является коньком аннотации @Value. Полная ее мощь заключается в возможности
+//    использовать выражения на языке SpEL. Это делает аннотацию @Value мощным инструментом внедрения. Например:
 
-//    Чтобы гарантировать, что Spring выберет пианино для компонента kenny, даже если существуют другие компоненты,
-//    которые могут быть связаны с его свойством instrument, можно добавить аннотацию @Qualifier, ссылающуюся на компонент
-//    с именем piano:
-    @Qualifier("piano")
+    @Value("#{saxophone}") // Внедрение компонента по его идентификатору saxophone, который определен в XML-конфигурации
     public void setInstrument(Instrument instrument) { // Внедрение
         this.instrument = instrument; // инструмент
     }
-
 
 
 
