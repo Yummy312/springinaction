9da8ebc2bbce80ffbabdbddb35a53bb7ddfdcba3
Yummy312 <kurmushuev.nurzhigit@gmail.com>
2025-02-18 23:51:24 +0600

    Автоматическое определение компонентов

--- src/main/java/com/springinaction/springidol/Instrumentalist.java
+++ src/main/java/com/springinaction/springidol/Instrumentalist.java
@@ -1,5 +1,6 @@
 package com.springinaction.springidol;
 import com.springinaction.springidol.exceptions.PerformanceException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 
@@ -13,17 +14,12 @@ public class Instrumentalist implements Performer {
     }
     private String song;
 
-//    Помимо внедрения ссылок на компоненты с помощью аннотаций автоматического связывания,
-//    может появиться желание использовать аннотации для внедрения простых значений. В версии Spring
-//    3.0 появилась новая аннотация @Value , позволяющая внедрять простые значения таких типов,
-//    как int, boolean и String. Например:
-    @Value("Jingle Bells")  // Здесь в строковое свойство внедряется обычное строковое значение
-    public void setSong(String song) { // Внедрение мелодии
+
+    @Value("Jingle Bells")
+    public void setSong(String song) {
         this.song = song;
     }
-//    Аннотация @Value проста в использовании. Ее применение заключается в том, чтобы добавить аннотацию
-//    @Value перед свойством, методом или параметром метода и передать ей строку с выражением,
-//    результат которого должен быть внедрен.
+
 
 
     public String getSong() {
@@ -34,13 +30,9 @@ public class Instrumentalist implements Performer {
     }
     private Instrument instrument;
 
-
-//    Внедрение простых значений не является коньком аннотации @Value. Полная ее мощь заключается в возможности
-//    использовать выражения на языке SpEL. Это делает аннотацию @Value мощным инструментом внедрения. Например:
-
-    @Value("#{saxophone}") // Внедрение компонента по его идентификатору saxophone, который определен в XML-конфигурации
-    public void setInstrument(Instrument instrument) { // Внедрение
-        this.instrument = instrument; // инструмент
+    @Autowired
+    public void setInstrument(Instrument instrument) {
+        this.instrument = instrument;
     }
 
 
